99cee579e91817b016460fa7e5829bcd2c169f69
Alex <alexlnetsch@gmail.com>
2020-10-16 16:44:57 -0700

    trader config, blackmarket add

---
+++ @VPPAdminTools/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1708571078;
+name = "VPPAdminTools";
+timestamp = 5249044586071480627;
--- Start Server.bat
+++ Start Server.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "DayZServer_x64" -instanceId=1 -config=serverDZ.cfg -mod=@CF;@Community-Online-Tools;@CPBWeapons;@Dayz-Expansion;@Dayz-Expansion-Licensed;@mvs;@Summer_Chernarus;@Trader -profiles=ServerName -port=2302 -cpuCount=4 -noFilePatching -dologs -adminlog -freezecheck
+start "" "DayZServer_x64" -instanceId=1 -config=serverDZ.cfg "-mod=@CF;@Community-Online-Tools;@CPBWeapons;@Dayz-Expansion;@Dayz-Expansion-Licensed;@mvs;@Summer_Chernarus;@Trader;@VPPAdminTools;" -profiles=ServerName -port=2302 -cpuCount=4 -noFilePatching -dologs -adminlog -freezecheck
